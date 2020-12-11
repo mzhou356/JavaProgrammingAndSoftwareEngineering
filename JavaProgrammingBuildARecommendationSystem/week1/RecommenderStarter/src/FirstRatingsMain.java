bec9c16a1f8bb3fbc99b85e360b46c59f4bb93ae
Mindy Zhou <minzhou16@gmail.com>
2020-12-11 17:23:26 -0500

    Load in movie and rating data
    Signed-off-by: Mindy Zhou <minzhou16@gmail.com>

---
+++ JavaProgrammingBuildARecommendationSystem/week1/RecommenderStarter/src/FirstRatingsMain.java
@@ -0,0 +1,9 @@
+public class FirstRatingsMain {
+    public static void main(String[] args){
+        FirstRatings firstRatings = new FirstRatings();
+        firstRatings.testLoadMovies("data/ratedmoviesfull.csv");
+        firstRatings.testLoadRaters("data/ratings.csv","193","1798709");
+
+
+    }
+}
